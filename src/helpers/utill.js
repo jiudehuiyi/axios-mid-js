a735aa005a0c4c203f538c551f1b14c2a51aabda
jiudehuiyi <136859304@qq.com>
2020-11-12 23:40:09 +0800

    add a isplainObject method

--- src/helpers/utill.js
+++ src/helpers/utill.js
@@ -11,12 +11,14 @@ export function isDate(val){
 //判断是否为对象
 export function isObject(val){
 
-    return utill_toString.call(val) === "[object Object]";
-    //或者
+    //这种方法返回的是各种对象，例如array，date，Regexp等
     return Object !== null && typeof val === "object";
 }
 
-
+//判断是否为纯对象({})
+export function isPlainObject(val){
+    return utill_toString.call(val) === "[object Object]";
+}
 
 
 
