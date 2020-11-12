165e2eb334b4a2e18bce03d390cc4199c99e7d39
jiudehuiyi <136859304@qq.com>
2020-11-12 23:25:46 +0800

    限制config的url配置

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 
 import xhr from "./xhr";
-
+import { buildURL } from "./helpers/url";
 function axios(config){
+
+    processConfig(config);
+
     xhr(config);
+}
+
+function processConfig(config) {
+    config.url = transformURL(config);
+}
+
+function transformURL(config){
+    const { url,params } = config;
+    return buildURL( url,params );
 }
 export default axios;
