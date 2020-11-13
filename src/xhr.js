2351ccd254eaa9e84eafe6050837b2b8f6b75927
jiudehuiyi <136859304@qq.com>
2020-11-13 14:43:24 +0800

    处理header逻辑

--- src/xhr.js
+++ src/xhr.js
@@ -4,9 +4,20 @@ function xhr(config){
 
     const xhr = new XMLHttpRequest();
 
-    const { method="get",url,data=null,...reset } = config;
+    const { method="get",url,data=null,headers,...reset } = config;
 
     xhr.open( method.toUpperCase(),url,true );
+
+//处理header逻辑
+Object.keys(headers).forEach( (name)=>{
+
+    if( data === null && name.toUpperCase() === "CONTENT-TYPE" ){
+        delete headers[name];
+    }else {
+        xhr.setRequestHeader( name,headers[name] );
+    }
+
+} );
 
     xhr.send(data);
 }
